8193e310a0ea6e24fadb4e1f8e2533ca1b876bcb
Floris Douw <fdouw@users.noreply.github.com>
2020-05-17 14:30:34 +0200

    Add initial version for 2015, day 3, part 2

--- Y2015/Day03.cs
+++ Y2015/Day03.cs
@@ -2,29 +2,45 @@ using System.Collections.Generic;
 
 namespace AdventOfCode.Y2015 {
     class Day03 : Solution {
-        HashSet<(int,int)> houses = new HashSet<(int, int)>();
 
         public Day03 () : base(3, 2015, "Perfectly Spherical Houses in a Vacuum") {
         }
 
         public override string SolvePart1 () {
+            HashSet<(int,int)> houses = new HashSet<(int, int)>();
             (int X,int Y) house = (X: 0, Y: 0);
             houses.Add(house);
             for (int i=0; i<Input[0].Length; i++) {
-                switch (Input[0][i]) {
-                    case '^': house = (house.X, house.Y - 1); break;
-                    case '>': house = (house.X + 1, house.Y); break;
-                    case '<': house = (house.X - 1, house.Y); break;
-                    case 'v': house = (house.X, house.Y + 1); break;
-                    default: throw new System.Exception($"Invalid direction: {Input[0][i]}");
-                }
+                house = Move(house, Input[0][i]);
                 houses.Add(house);
             }
             return $"{houses.Count}";
         }
 
         public override string SolvePart2 () {
-            return "Not yet implemented.";
+            HashSet<(int,int)> houses = new HashSet<(int, int)>();
+            (int X,int Y) santa = (X: 0, Y: 0);
+            (int X,int Y) robot = (X: 0, Y: 0);
+            houses.Add(santa);  // Robot is still the same
+            for (int i=0; i<Input[0].Length; i+=2) {
+                santa = Move(santa, Input[0][i]);
+                houses.Add(santa);
+            }
+            for (int i=1; i<Input[0].Length; i+=2) {
+                robot = Move(robot, Input[0][i]);
+                houses.Add(robot);
+            }
+            return $"{houses.Count}";
+        }
+
+        private (int X, int Y) Move ((int X, int Y) oldPos, char dir) {
+            switch (dir) {
+                case '^': return (oldPos.X, oldPos.Y - 1);
+                case '>': return (oldPos.X + 1, oldPos.Y);
+                case '<': return (oldPos.X - 1, oldPos.Y);
+                case 'v': return (oldPos.X, oldPos.Y + 1);
+                default: throw new System.Exception($"Invalid direction: {dir}");
+            }
         }
     }
 }
