5dacd75a99fb6fa07ab9371fd90614bde71f45be
Floris Douw <fdouw@users.noreply.github.com>
2020-05-17 17:10:17 +0200

    Add initial version for 2015, day 5, part 2

--- Y2015/Day05.cs
+++ Y2015/Day05.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 
 namespace AdventOfCode.Y2015 {
     class Day05 : Solution {
@@ -29,11 +30,42 @@ namespace AdventOfCode.Y2015 {
                 }
             }
             return $"{niceCount}";
-            
+
             bool isVowel (char c) => (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
         }
 
-        // public override string SolvePart2 () {
-        // }
+        public override string SolvePart2 () {
+            int niceCount = 0;
+            Dictionary<string,int> pairDict = new Dictionary<string, int>();
+            foreach (string word in Input) {
+                bool repeatPair = false;
+                bool repeatChar = false;
+                string pair;
+                pairDict.Clear();
+                pairDict.Add($"{word[0]}{word[1]}", 0); // Skipped in the loop
+                for (int i = 2; i < word.Length; i++) {
+                    if (word[i] == word[i-2]) {
+                        repeatChar = true;
+                        if (repeatPair) break;  // Found both conditions, so we can stop
+                    }
+                    if (!repeatPair) {  // Avoid needless checks
+                        pair = $"{word[i-1]}{word[i]}";
+                        if (pairDict.ContainsKey(pair)) {
+                            if (pairDict[pair] < i-2) { // Ignore overlap
+                                repeatPair = true;
+                                if (repeatChar) break;  // Found both conditions, so we can stop
+                            }
+                        }
+                        else { // !pairDict.ContainsKey(pair)
+                            pairDict.Add(pair, i-1);
+                        }
+                    }
+                }
+                if (repeatChar && repeatPair) {
+                    niceCount++;
+                }
+            }
+            return $"{niceCount}";
+        }
     }
 }
