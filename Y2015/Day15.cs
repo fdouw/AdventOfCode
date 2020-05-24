b7fb40cc99628b314d63224ed545a62b6d871c33
Floris Douw <fdouw@users.noreply.github.com>
2020-05-24 19:14:28 +0200

    Add initial solution to 2015, day 15, part 2

--- Y2015/Day15.cs
+++ Y2015/Day15.cs
@@ -7,17 +7,7 @@ namespace AdventOfCode.Y2015 {
         }
 
         public override string SolvePart1 () {
-            var ingredients = new List<Ingredient>();
-            foreach (string line in Input) {
-                string[] data = line.Split(' ');
-                // Assume data are in fixed fields, and all have : or , to remove.
-                string name = data[0].Substring(0, data[0].Length - 1);
-                int capacity = Int32.Parse(data[2].Substring(0, data[2].Length - 1));
-                int durability = Int32.Parse(data[4].Substring(0, data[4].Length - 1));
-                int flavour = Int32.Parse(data[6].Substring(0, data[6].Length - 1));
-                int texture = Int32.Parse(data[8].Substring(0, data[8].Length - 1));
-                ingredients.Add(new Ingredient(name, capacity, durability, flavour, texture));
-            }
+            List<Ingredient> ingredients = GetIngredients();
 
             // Compute the highest combination
             int maxValue = 0;
@@ -36,8 +26,42 @@ namespace AdventOfCode.Y2015 {
             return $"{maxValue}";
         }
 
-        // public override string SolvePart2 () {
-        // }
+        public override string SolvePart2 () {
+            List<Ingredient> ingredients = GetIngredients();
+
+            // Compute the highest combination
+            int maxValue = 0;
+            int spoonsAvailable = 100;  // Given in problem
+            int targetCalories = 500;   // Given in problem
+            var list = new List<int>();
+            foreach (List<int> weights in Partition(ingredients.Count, spoonsAvailable, list)) {
+                Ingredient sum = weights[0] * ingredients[0];   // Assuming non-empty lists
+                for (int i = 1; i < ingredients.Count; i++) {
+                    sum += weights[i] * ingredients[i];
+                }
+                if (sum.Calories == targetCalories && sum.Value > maxValue) {
+                    maxValue = sum.Value;
+                }
+            }
+
+            return $"{maxValue}";
+        }
+
+        private List<Ingredient> GetIngredients () {
+            var ingredients = new List<Ingredient>();
+            foreach (string line in Input) {
+                string[] data = line.Split(' ');
+                // Assume data are in fixed fields, and all have : or , to remove, except calories
+                string name = data[0].Substring(0, data[0].Length - 1);
+                int capacity = Int32.Parse(data[2].Substring(0, data[2].Length - 1));
+                int durability = Int32.Parse(data[4].Substring(0, data[4].Length - 1));
+                int flavour = Int32.Parse(data[6].Substring(0, data[6].Length - 1));
+                int texture = Int32.Parse(data[8].Substring(0, data[8].Length - 1));
+                int calories = Int32.Parse(data[10]);
+                ingredients.Add(new Ingredient(name, capacity, durability, flavour, texture, calories));
+            }
+            return ingredients;
+        }
 
         /* Returns all the partitions of <limit> items into <nparts> parts. <list> is used as
          * storage during recursion. */
@@ -69,14 +93,16 @@ namespace AdventOfCode.Y2015 {
         internal int Durability { get; }
         internal int Flavour { get; }
         internal int Texture { get; }
+        internal int Calories { get; }
         internal int Value { get => max(Capacity, 0) * max(Durability, 0) * max(Flavour, 0) * max(Texture, 0); }
 
-        internal Ingredient (string n, int cap, int dur, int flav, int text) {
+        internal Ingredient (string n, int cap, int dur, int flav, int text, int cal = 0) {
             Name = n;
             Capacity = cap;
             Durability = dur;
             Flavour = flav;
             Texture = text;
+            Calories = cal;
         }
 
         public static Ingredient operator + (Ingredient a, Ingredient b) {
@@ -84,7 +110,8 @@ namespace AdventOfCode.Y2015 {
                                   a.Capacity + b.Capacity,
                                   a.Durability + b.Durability,
                                   a.Flavour + b.Flavour,
-                                  a.Texture + b.Texture);
+                                  a.Texture + b.Texture,
+                                  a.Calories + b.Calories);
         }
 
         public static Ingredient operator * (int n, Ingredient ingredient) {
@@ -92,7 +119,8 @@ namespace AdventOfCode.Y2015 {
                                   n * ingredient.Capacity,
                                   n * ingredient.Durability,
                                   n * ingredient.Flavour,
-                                  n * ingredient.Texture);
+                                  n * ingredient.Texture,
+                                  n * ingredient.Calories);
         }
 
         private static int max (int a, int b) => (a > b) ? a : b;
